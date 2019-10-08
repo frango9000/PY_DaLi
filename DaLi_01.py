cc19e5f61789f95e6567060864f85f54e4b69c72
fsancheztemprano <fsancheztemprano@danielcastelao.org>
2019-10-08 13:40:56 +0200

    ex01 p1

---
+++ DaLi_01.py
@@ -0,0 +1,36 @@
+def ex01():
+    print("Welcome to Python")
+    print("Learning Java Now")
+    print("Programming is fun")
+
+
+def ex02():
+    loops = 5
+    while loops > 0:
+        print("I love Python")
+        loops -= 1
+
+
+def ex04():
+    print("a   a^2  a^3 a^4")
+    loop = 1
+
+    while loop < 5:
+        print("{:} {:4} {:4} {:4}".format(loop ** 1, loop ** 2, loop ** 3, loop ** 4))
+        loop += 1
+
+
+def ex05():
+    print(((7.5 * 6.5) - (4.5 * 3)) / (47.5 - 5.5))
+
+
+def ex06():
+    loop = 1
+    sum = 0
+    while loop < 10:
+        sum += loop
+        loop += 1
+    print(sum)
+
+
+ex06()
